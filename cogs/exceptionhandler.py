efd836013605dab888214865b9ee4127e2e7f3e9
mpeiffer <mbp83@nau.edu>
2022-10-06 17:11:11 -0700

    Set the title message on each error

--- cogs/exceptionhandler.py
+++ cogs/exceptionhandler.py
@@ -49,19 +49,43 @@ class ExceptionHandler(commands.Cog):
 
 
         # BadLiteralArgument
-        # ChannelNotFound
-        # LargeIntConversionFailure
-        # MemberNotFound
-        # MissingPermissions
-        # MissingRequiredArgument
-        # RoleNotFound
-        # UserNotFound
+        if isinstance(error, commands.errors.BadLiteralArgument):
+            embed.set_title('Sorry, I couldn\'t read your input.')
 
+        # ChannelNotFound
+        if isinstance(error, commands.errors.ChannelNotFound):
+            embed.set_title('Uh oh! It looks like that channel doesn\'t exist.')
+
+        # LargeIntConversionFailure
+        if isinstance(error, commands.errors.LargeIntConversionFailure):
+            embed.set_title('Sorry, I can\'t count that high.')
+
+        # MemberNotFound
+        if isinstance(error, commands.errors.MemberNotFound):
+            embed.set_title('I couldn\'t find a member with that name.')
+
+        # MissingPermissions
+        if isinstance(error, commands.errors.MissingPermissions):
+            embed.set_title('Nice try! You don\'t have permission to do that.')
+
+        # MissingRequiredArgument
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            embed.set_title('Don\'t leave me hanging. It looks like you\'re missing a field.')
+
+        # MissingRole
         if (
             isinstance(error, commands.errors.MissingRole)
             or isinstance(error, commands.errors.MissingAnyRole)
         ):
             embed.set_title('Whoops! You\'re missing a role.')
+
+        # RoleNotFound
+        if isinstance(error, commands.errors.RoleNotFound):
+            embed.set_title('I couldn\'t find that role.')
+
+        # UserNotFound
+        if isinstance(error, commands.errors.UserNotFound):
+            embed.set_title('I don\'t know who that is.')
 
         else:
             embed.set_title('Whoops! An alien error occured.')
