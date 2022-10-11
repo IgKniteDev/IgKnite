dc56042f843873be407ecce97a47233d6c9d590f
HitBlast <67194380+hitblast@users.noreply.github.com>
2022-10-11 19:45:43 +0600

    Reduce self-bots and improved code style.

--- cogs/general.py
+++ cogs/general.py
@@ -20,12 +20,10 @@ from core import global_
 from core.embeds import TypicalEmbed
 
 
-# Backend for the `ping` command.
-async def _ping_backend(
-    inter: disnake.CommandInteraction,
-    bot: core.IgKnite,
-) -> TypicalEmbed:
-    system_latency = round(bot.latency * 1000)
+# Backend for userinfo-labelled commands.
+# Do not use it within other commands unless really necessary.
+async def _ping_backend(inter: disnake.CommandInteraction) -> TypicalEmbed:
+    system_latency = round(inter.bot.latency * 1000)
 
     start_time = time.time()
     await inter.response.defer()
@@ -38,7 +36,7 @@ async def _ping_backend(
 
     embed = core.TypicalEmbed(inter).add_field(
         name='System Latency',
-        value=f'{system_latency}ms [{bot.shard_count} shard(s)]',
+        value=f'{system_latency}ms [{inter.bot.shard_count} shard(s)]',
         inline=False
     ).add_field(
         name='API Latency',
@@ -57,20 +55,18 @@ class PingCommandView(disnake.ui.View):
     def __init__(
         self,
         inter: disnake.CommandInteraction,
-        bot: core.IgKnite,
         timeout: float = 60
     ) -> None:
         super().__init__(timeout=timeout)
         self.inter = inter
-        self.bot = bot
 
-    @disnake.ui.button(emoji='🔄', style=disnake.ButtonStyle.green)
+    @disnake.ui.button(label='Refresh', style=disnake.ButtonStyle.gray)
     async def _refresh(
         self,
-        button: disnake.ui.Button,
+        _: disnake.ui.Button,
         inter: disnake.Interaction
     ) -> None:
-        embed = await _ping_backend(bot=self.bot, inter=inter)
+        embed = await _ping_backend(inter)
         await inter.edit_original_message(embed=embed, view=self)
 
 
@@ -137,9 +133,8 @@ class General(commands.Cog):
         self,
         inter: disnake.CommandInteraction
     ) -> None:
-        view = PingCommandView(bot=self.bot, inter=inter)
-        embed = await _ping_backend(bot=self.bot, inter=inter)
-        await inter.send(embed=embed, view=view)
+        embed = await _ping_backend(inter)
+        await inter.send(embed=embed, view=PingCommandView(inter=inter))
 
 
 # The setup() function for the cog.
