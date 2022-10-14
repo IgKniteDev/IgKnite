d39c48ea5f41d157da0484a59df666a8d2ce764d
mpeiffer <mbp83@nau.edu>
2022-10-13 17:37:42 -0700

    Update with accurate exceptions and handler other command types

--- cogs/exceptionhandler.py
+++ cogs/exceptionhandler.py
@@ -12,6 +12,7 @@ from typing import Any
 
 import disnake
 from disnake.ext import commands
+from disnake import errors
 
 import core
 
@@ -32,50 +33,82 @@ class ExceptionHandler(commands.Cog):
     ) -> None:
         error = getattr(error, 'original', error)
         embed = core.TypicalEmbed(inter, is_error=True)
-
-
-        # BadLiteralArgument
-        if isinstance(error, commands.errors.BadLiteralArgument):
-            embed.set_title('Sorry, I couldn\'t read your input.')
-
-        # ChannelNotFound
-        if isinstance(error, commands.errors.ChannelNotFound):
-            embed.set_title('Uh oh! It looks like that channel doesn\'t exist.')
-
-        # LargeIntConversionFailure
-        if isinstance(error, commands.errors.LargeIntConversionFailure):
-            embed.set_title('Sorry, I can\'t count that high.')
-
-        # MemberNotFound
-        if isinstance(error, commands.errors.MemberNotFound):
-            embed.set_title('I couldn\'t find a member with that name.')
-
+    
         # MissingPermissions
-        if isinstance(error, commands.errors.MissingPermissions):
+        if (
+            isinstance(error, commands.errors.MissingPermissions)
+            or isinstance(error, errors.Forbidden)
+        ):
             embed.set_title('Nice try! You don\'t have permission to do that.')
 
-        # MissingRequiredArgument
-        if isinstance(error, commands.errors.MissingRequiredArgument):
-            embed.set_title('Don\'t leave me hanging. It looks like you\'re missing a field.')
-
         # MissingRole
-        if (
+        elif (
             isinstance(error, commands.errors.MissingRole)
             or isinstance(error, commands.errors.MissingAnyRole)
         ):
             embed.set_title('Whoops! You\'re missing a role.')
 
-        # RoleNotFound
-        if isinstance(error, commands.errors.RoleNotFound):
-            embed.set_title('I couldn\'t find that role.')
+        else:
+            embed.set_title('Whoops! An alien error occured.')
+        
+        embed.set_description(str(error))
+        await inter.send(embed=embed, ephemeral=True)
+    
+    @commands.Cog.listener()
+    async def on_user_command_error(
+        self,
+        inter: disnake.CommandInteraction,
+        error: Any
+    ) -> None:
+        error = getattr(error, 'original', error)
+        embed = core.TypicalEmbed(inter, is_error=True)
 
-        # UserNotFound
-        if isinstance(error, commands.errors.UserNotFound):
-            embed.set_title('I don\'t know who that is.')
+        # MissingPermissions
+        if (
+            isinstance(error, commands.errors.MissingPermissions)
+            or isinstance(error, errors.Forbidden)
+        ):
+            embed.set_title('Nice try! You don\'t have permission to do that.')
+
+        # MissingRole
+        elif (
+            isinstance(error, commands.errors.MissingRole)
+            or isinstance(error, commands.errors.MissingAnyRole)
+        ):
+            embed.set_title('Whoops! You\'re missing a role.')
 
         else:
             embed.set_title('Whoops! An alien error occured.')
+        
+        embed.set_description(str(error))
+        await inter.send(embed=embed, ephemeral=True)
 
+    @commands.Cog.listener()
+    async def on_message_command_error(
+        self,
+        inter: disnake.CommandInteraction,
+        error: Any
+    ) -> None:
+        error = getattr(error, 'original', error)
+        embed = core.TypicalEmbed(inter, is_error=True)
+    
+        # MissingPermissions
+        if (
+            isinstance(error, commands.errors.MissingPermissions)
+            or isinstance(error, errors.Forbidden)
+        ):
+            embed.set_title('Nice try! You don\'t have permission to do that.')
+
+        # MissingRole
+        elif (
+            isinstance(error, commands.errors.MissingRole)
+            or isinstance(error, commands.errors.MissingAnyRole)
+        ):
+            embed.set_title('Whoops! You\'re missing a role.')
+
+        else:
+            embed.set_title('Whoops! An alien error occured.')
+        
         embed.set_description(str(error))
         await inter.send(embed=embed, ephemeral=True)
 
