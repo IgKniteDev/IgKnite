6f503204f5e7af79fee5c2fbcb2b501dcde6c50d
HitBlast <67194380+hitblast@users.noreply.github.com>
2023-02-22 13:50:05 +0600

    bumped version date

--- core/__init__.py
+++ core/__init__.py
@@ -16,7 +16,7 @@ from .bot import *
 from .embeds import *
 
 # Set version number.
-__version_info__ = ('2022', '12', '31')  # Year.Month.Day
+__version_info__ = ('2022', '2', '22')  # Year.Month.Day
 __version__ = '.'.join(__version_info__)
 
 # Set bot metadata.
