cb699af642cbdce6f5c35b59bd054998d7106767
HitBlast <hitblastlive@gmail.com>
2023-04-01 00:03:25 +0600

    bumped version

--- core/__init__.py
+++ core/__init__.py
@@ -16,7 +16,7 @@ from .bot import *
 from .ui import *
 
 # Set version number.
-__version_info__ = ('2023', '3', '31')  # Year.Month.Day
+__version_info__ = ('2023', '4', '1')  # Year.Month.Day
 __version__ = '.'.join(__version_info__)
 
 # Set bot metadata.
