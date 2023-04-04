81202cd4be013d15057d68bce63f3642e060ef42
HitBlast <hitblastlive@gmail.com>
2023-04-04 22:03:50 +0600

    updated version number

--- core/__init__.py
+++ core/__init__.py
@@ -17,7 +17,7 @@ from .bot import *
 from .ui import *
 
 # Set version number.
-__version_info__ = ('2023', '4', '1')  # Year.Month.Day
+__version_info__ = ('2023', '4', '4')  # Year.Month.Day
 __version__ = '.'.join(__version_info__)
 
 
